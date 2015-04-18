716cc77fcaf6e9eed106c0bbcf9325a45fcb6294
shaocong_ye <shaocong_ye@hotmail.com>
2015-04-18 10:25:13 +0800

    Initial Commit

---
+++ Descom_LED_Control/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Descom_LED_Control
+//
+//  Created by mac book on 15-4-18.
+//  Copyright (c) 2015年 jinslight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
