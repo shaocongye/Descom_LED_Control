d507d43a7c7203282464095ceb24656163d45361
shaocong_ye <shaocong_ye@hotmail.com>
2015-05-13 13:51:39 +0800

    text

--- Descom_LED_Control/AppDelegate.h
+++ Descom_LED_Control/AppDelegate.h
@@ -7,16 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MainViewController.h"
+#import "DataModuleControl.h"
+#import "BLENetworkControl.h"
+#import "ASIHTTPRequestDelegate.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#define registerURL @"http://www.jinslight.com:8080/LightServer/register"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,ASIHTTPRequestDelegate>
+{
+    DataModuleControl *_config;
+    
+    UIImageView  *_SplashImage;
+    UIView *_SplashView;
+    
+    int _stop;
+}
+
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) MainViewController *mainViewController;
 
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (strong, nonatomic) UINavigationController *viewController;
+//@property (strong, nonatomic) UINavigationController *navigationController;
+
+@property (strong, nonatomic) BLENetworkControl *BLENetworkControl;
+@property (strong, nonatomic) DataModuleControl *config;
+
 
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
+
+- (float)getIOSVersion;
+- (void)savedUUID:(NSString *)uuid;
+
+- (NSString *)fetchUUID;
+
+-(void)reachabilityChanged:(NSNotification*)note;
+
+
+
+
 
 @end
--- Descom_LED_Control/AppDelegate.m
+++ Descom_LED_Control/AppDelegate.m
@@ -5,51 +5,164 @@
 //  Created by mac book on 15-4-18.
 //  Copyright (c) 2015年 jinslight. All rights reserved.
 //
+#import <Foundation/Foundation.h>
 
 #import "AppDelegate.h"
+#import "Reachability.h"
+#import "ASIHTTPRequest.h"
+#import "NSString+SBJSON.h"
+#import "ServerData.h"
+#import "MainWindowNavbar.h"
+#import "ServerDataPaser.h"
+#import "SSKeychain.h"
+
 
 @implementation AppDelegate
-
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize managedObjectModel = _managedObjectModel;
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+@synthesize config = _config;
+@synthesize mainViewController;
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    _stop = 0;
+    
+    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    _config = [[DataModuleControl alloc]init];
+    
+    //然后添加一个视图进去，否则导航栏也没有意义的
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        self.mainViewController = [[MainViewController alloc] initWithNibName:@"MainView_IPhone" bundle:nil];
+    } else {
+        self.mainViewController = [[MainViewController alloc] initWithNibName:@"MainView_IPad" bundle:nil];
+    }
+    
+    [self.mainViewController setConfig: _config.config];
+    
+    
+    
+    UINavigationController *nav = [[UINavigationController alloc] initWithNavigationBarClass:[MainWindowNavbar class] toolbarClass:nil];
+    
+    nav.viewControllers = @[mainViewController];
+
+    self.viewController.navigationBarHidden = NO;
+    
+    self.window.rootViewController = nav;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
+    
+    //初始化蓝牙
+    self.BLENetworkControl = [[BLENetworkControl alloc] initWithUUID:nil];
+    self.BLENetworkControl.delegate = self.mainViewController;
+    
+    _SplashImage =[[UIImageView alloc]initWithFrame:self.window.frame];//初始化fView
+    _SplashImage.image=[UIImage imageNamed:@"splash.png"];//图片f.png 到fView
+    
+    
+    _SplashView = [[UIView alloc]initWithFrame:self.window.frame];//初始化rView
+    
+    [_SplashView addSubview:_SplashImage];//add 到rView
+    
+    [self.window addSubview:_SplashView];//add 到window
+    
+    [self performSelector:@selector(TheAnimation) withObject:nil afterDelay:2];//动画，2秒钟后切换到主界面
+    
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reachabilityChanged:)
+                                                 name:kReachabilityChangedNotification
+                                               object:nil];
+    
+    
     return YES;
 }
 
+
+//从活动状态到后台
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    _stop =1;
 }
 
+//程序进入后台状态的时候
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    _stop = 1;
+    
+    [self.BLENetworkControl ConnectedBLEPrepharal:FALSE];
 }
 
+//程序从后台进入活动状态
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
+//程序从挂起状态进入活动状态
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   
+    NSLog(@"applicationDidBecomeActive");
+    
+    _stop = 0;
+    //启动网络线程，如果没有连接上，一直连
+    [self.BLENetworkControl ScanBLEPrepharal];
+    
+    [self performSelector:@selector(TheAnimation) withObject:nil afterDelay:2];//动画，2秒钟后切换到主界面
+    
+    
+    //然后检测网络状态
+    Reachability * reach = [Reachability reachabilityWithHostname:@"www.jinslight.com"];
+    
+    //线程块  如果有连线消息就打印连线
+    reach.reachableBlock = ^(Reachability * reachability)
+    {
+        //异步线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //blockLabel.text = @"Block Says Reachable";
+            
+            NSLog(@"Block Says Reachable");
+            
+            //启动沟通线程进行工作
+            //            _stop = 0;
+        });
+    };
+    
+    //线程块  如果有断线消息就打印断线
+    reach.unreachableBlock = ^(Reachability * reachability)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //blockLabel.text = @"Block Says Unreachable";
+            NSLog(@"Block Says Unreachable");
+            
+            //关闭沟通线程
+            _stop = 1;
+            
+        });
+    };
+    
+    [reach startNotifier];
 }
 
+//退出的时候自动保存配置文件
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Saves changes in the application's managed object context before the application terminates.
+    
+    NSLog(@"applicationWillEnterForeground");
+    
     [self saveContext];
 }
+
 
 - (void)saveContext
 {
@@ -145,5 +258,225 @@
 {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
+
+
+
+
+//动画，2秒钟后切换到主界面
+- (void)TheAnimation{
+    
+    CATransition *animation = [CATransition animation];
+    animation.delegate = self;
+    animation.duration = 0.7 ;  // 动画持续时间(秒)
+    animation.timingFunction = UIViewAnimationCurveEaseInOut;
+    animation.type = kCATransitionFade;//淡入淡出效果
+    
+    [UIView animateWithDuration:0.7 //速度0.7秒
+                     animations:^{//修改rView坐标
+                         _SplashView.frame = CGRectMake(self.window.frame.origin.x,
+                                                        -self.window.frame.size.height,
+                                                        self.window.frame.size.width,
+                                                        self.window.frame.size.height);
+                     }
+                     completion:^(BOOL finished){
+                     }];
+    
+}
+
+
+
+static void uncaughtExceptionHandler(NSException *exception) {
+    
+    NSLog(@"CRASH: %@",exception);
+    
+    NSLog(@"Stack Trace: %@",[exception callStackSymbols]);
+    
+    // Internal error reporting
+}
+
+
+-(void)reachabilityChanged:(NSNotification*)note
+{
+    Reachability * reach = [note object];
+    
+    if([reach isReachable])
+    {
+        //        notificationLabel.text = @"Notification Says Reachable";
+        NSLog(@"Notification Says Reachable");
+        _stop = 0;
+        
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_async(queue, ^{
+            //启动沟通线程进行工作
+            _stop = 0;
+            
+            [self listenServer];
+        });
+    }
+    else
+    {
+        //        notificationLabel.text = @"Notification Says Unreachable";
+        NSLog(@"Notification Says Unreachable");
+        _stop = 1;
+    }
+}
+
+
+
+//与服务器沟通，下载数据，注册，上传数据等
+-(void) listenServer{
+    for(int i = 0; i < 1; i++)
+    {
+        //后台的时候不干活
+        if(_stop)
+            return;
+        
+        //如果是第一次登录就注册
+        NSString *uuid = [self fetchUUID ];
+        if(uuid == nil)
+        {
+            //if(!_config.config.route.first)
+            
+            //构造链接
+            NSMutableString *URL = [NSMutableString stringWithString:registerURL];
+            //获取版本号
+            [URL appendString:@"?version=1.2.1000"];
+            
+            //获取os
+            NSString *os = [NSString stringWithFormat:@"&os=ios%f",[self getIOSVersion]];
+            [URL appendString:os];
+            
+            //获取uuid
+            NSString *uuidstr = [self createUUID];
+            
+            if(uuidstr.length>0)
+            {
+                [URL appendString:@"&uuid="];
+                [URL appendString:uuidstr];
+            }
+            
+            //获取mobile number
+            NSString *mobile = [[NSUserDefaults standardUserDefaults] stringForKey:@"SBFormattedPhoneNumber"];
+            
+            if(mobile.length>0){
+                [URL appendString:@"&mobile="];
+                [URL appendString:mobile];
+            }
+            
+            //获取用户类型
+            [URL appendString:@"&type=1"];
+            
+            
+            ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:URL]];
+            
+            request.delegate = self;
+            [request startAsynchronous];
+        }
+        
+        
+        //上传新设备mac或uuid
+        //....
+        
+        //检查新广告版本
+        
+        [NSThread sleepForTimeInterval:30];
+    }
+}
+
+#pragma mark - ASIHTTPRequestDelegate
+//获取版本号
+- (float)getIOSVersion
+{
+    return [[[UIDevice currentDevice] systemVersion] floatValue];
+}
+
+
+-(NSString*) createUUID {
+    CFUUIDRef  uuidObj = CFUUIDCreate(nil);//create a new UUID
+    NSString  *uuidString = (__bridge_transfer NSString *)CFUUIDCreateString(nil, uuidObj);
+    CFRelease(uuidObj);
+    return uuidString ;
+}
+
+
+- (void)savedUUID:(NSString *)uuid {
+    SSKeychainQuery *query = [[SSKeychainQuery alloc] init];
+    NSError *error;
+	query.password = uuid;
+    query.service = @"jinslight";
+    query.account = @"shaocong_ye";
+    query.synchronizationMode  = SSKeychainQuerySynchronizationModeYes;
+    
+    [query save:&error];
+}
+
+
+- (NSString *)fetchUUID {
+    SSKeychainQuery *query = [[SSKeychainQuery alloc] init];
+    NSError *error;
+    
+    query.service = @"jinslight";
+    query.account = @"shaocong_ye";
+    query.synchronizationMode  = SSKeychainQuerySynchronizationModeYes;
+    
+    query.password = nil;
+    
+    if ([query fetch:&error]) {
+        return query.password;
+    }
+    return nil;
+}
+
+//解析JSON
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    NSMutableDictionary *jsonDict = [request.responseString  JSONValue];
+    NSLog(@"request  : %@",request.responseString);
+    
+    
+    //解析注册信息
+    ServerDataPaser *paser =  [[ServerDataPaser alloc] init];
+    
+    MobileUser *user =[paser loadMobileUserRegister:jsonDict];
+    if(user != nil)
+    {
+        
+        //注册完以后需要保存到配置文件中
+        [self savedUUID:user.uuid];
+        
+        _config.config.route.first = 1;
+        _config.config.route.uuid = user.uuid;
+        _config.config.route.serial = user.mid;
+        [_config saveConfig];
+    }
+    
+    
+    
+    //    NSMutableDictionary *userArray = [jsonDict objectForKey:@"MobileUser"];
+    
+    //    if(userArray)
+    {
+        
+        
+        //注册完以后需要保存到配置文件中
+        //_config
+    }
+    
+    //解析广告版本信息
+    
+    //解析控制信息
+    
+    
+    
+}
+
+//接收数据失败,失败了就停止线程工作
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+    NSLog(@"请求失败");
+    _stop = 1;
+}
+
+
 
 @end
